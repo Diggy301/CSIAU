bd46ba4c55045890862dc44cb2f3a969cbe90d1b
João Pedro Oliveira <75614916+Diggy301@users.noreply.github.com>
2021-08-31 13:32:36 +0100

    99% DONE
    circle Move done.

--- stageControl/src/csiau_code.py
+++ stageControl/src/csiau_code.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 #_*_coding:utf-8_*_
 
 
@@ -13,7 +14,9 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from math import pow, atan2, sqrt, radians, pi, atan
 import numpy as np
 
+
 #map limits: (-25,-25) to (25, 25)
+
 
 class Robot:
 	def __init__(self):
@@ -24,7 +27,7 @@ class Robot:
 		self.odom_sub = rospy.Subscriber('/odom', Odometry, self.callback_odom)
 		self.vel_msg = Twist()
 		self.rate = rospy.Rate(10)
-		
+
 		self.GOALSEEK = 1
 		self.WALLFOLLOW = 2
 		self.CIRCLEMOVE = 3
@@ -34,13 +37,19 @@ class Robot:
 		self.robot_y = 0
 		self.robot_theta = 0
 		self.laser = []
-		self.waypoint_x, self.waypoint_y = 0, 0
+		self.waypoint_x, self.waypoint_y = 0,0
 		self.cont = 0
 		self.state = self.GOALSEEK
 		self.last_angular_vel = 0
-	
 		
+
+		self.circlemove_values = []
+		self.circlemove_vcont = 0
+
+	
+
 		#self.get_waypoint()
+
 
 
 	def callback_laser(self, msg):
@@ -48,6 +57,7 @@ class Robot:
 
 	def callback_gas(self, msg):
 		self.gas = float(msg.data)
+
 
 	def callback_odom(self, msg):
 		"""
@@ -62,17 +72,20 @@ class Robot:
 		(roll, pitch, yaw) = euler_from_quaternion(orientation_list)
 		self.robot_theta = yaw
 
+
+
 	def move(self):
 		# while gas == 0
 			# move to random point
-		# circle move
-		# move to best direction whlie avoiding walls
+		# circle move 		
 		
-		# after a while do circle move again and repeat untili gas 100 +- 1		
-	
+		if self.gas > 98: # can be higher 
+			print('found GAS')
+
 		if len(self.laser): # wait for first laser read
 			if (self.state == self.GOALSEEK) or (self.state == self.WALLFOLLOW):
 				self.move_to_waypoint()
+
 			if self.state == self.CIRCLEMOVE:
 				self.circle_move()
 
@@ -80,30 +93,36 @@ class Robot:
 		self.last_angular_vel = self.vel_msg.angular.z
 
 
-	def check_waypoint(self):
+
+	def check_waypoint(self, tolerance):
 		dx = self.robot_x - self.waypoint_x
 		dy = self.robot_y - self.waypoint_y
 		d = sqrt(dx**2 + dy**2)
-		if d < 0.2:
+
+		if d < tolerance:
 			return True
 		return False
+
 	
-	def  get_waypoint(self):
+	def get_waypoint(self):
+
 		self.waypoint_x, self.waypoint_y = np.random.uniform(-25,25,[1,2])[0]
 
-	def move_to_waypoint(self):
-		if not self.check_waypoint():
-			waypoint_direction_d = self.get_angle_to_waypoint()
 
+
+	def move_to_waypoint(self):
+		if not self.check_waypoint(0.2):
+
+			waypoint_direction_d = self.get_angle_to_waypoint()
 			self.get_linear_vel()
 
 			if self.state == self.GOALSEEK:
 				#print('GOALSEEK')
 				self.get_goalseek_rot(waypoint_direction_d)
-		
+
 				if self.check_path_blocked(0):
 					self.state = self.WALLFOLLOW
-			
+
 			if self.state == self.WALLFOLLOW:
 				#print('WALLFOLLOW')	
 				self.get_wallfollow_rot(waypoint_direction_d)	
@@ -112,10 +131,12 @@ class Robot:
 					self.state = self.GOALSEEK				
 		else:
 			self.state = self.CIRCLEMOVE
-			self.circlemove_init()
-			print('arrived')
 
+			self.circlemove_init()
+
+			print('circle move START')
 	
+
 	def get_linear_vel(self):
 		"""
 		Give linear velocity to robot. Values chosen randomly.
@@ -127,9 +148,11 @@ class Robot:
 		dy = self.robot_y-self.waypoint_y
 		dx = self.robot_x-self.waypoint_x
 		d = sqrt(dx**2 + dy**2)
+
 		
 		self.vel_msg.linear.x = Kp*d + K
-	
+
+
 	def get_goalseek_rot(self, ang):
 		"""
 		Give angular velocity to robot in goalseek mode.
@@ -138,14 +161,15 @@ class Robot:
 		In order to smooth rotations the angular velocity is also
 		weighted with the angular velocity of the previous iteration.
 		"""
+
 		Kp = 2
+
 		if abs(ang) < 5:#15
 			self.vel_msg.angular.z = 0
+
 		else:
 			w = Kp*ang*pi/180
 			self.vel_msg.angular.z = 0.6*self.last_angular_vel + 0.4*w
-
-
 
 
 	def get_wallfollow_rot(self, ang):
@@ -156,25 +180,25 @@ class Robot:
 		In order to smooth rotations the angular velocity is also
 		weighted with the angular velocity of the previous iteration.
 		"""
-		
 		# Get best angle according to surroundings and desired direction
+
 		best_ang = self.get_closest_opening(135+ang)
 
-
 		if abs(best_ang-135) < 5:
-			self.vel_msg.angular.z = 0
 
+			self.vel_msg.angular.z = 0
 		else:
 			Kp = 1
 			w = Kp*(best_ang-135)*pi/180
-			
+
 			if abs(w-self.last_angular_vel) > 3:
 				# if change in angular vel is too big, ignore
 				self.vel_msg.angular.z = self.last_angular_vel
 			else:
-				self.vel_msg.angular.z = 0.6*self.last_angular_vel + 0.4*w
 
+				self.vel_msg.angular.z = 0.6*self.last_angular_vel + 0.4*w
 		
+
 	def get_closest_opening(self, angle):
 		"""
 		Function to return optimal angle. Optimal angle is the
@@ -183,9 +207,9 @@ class Robot:
 		
 		# 0's for blocked and 1's for open
 		binary_map = [0 if value < 3.5 else 1 for value in self.laser]
-
 		# if straight path (+- 15deg) is open to straigth
 		mid = 135
+
 		if all(l == 1  for l in binary_map[mid-15:mid+16]):
         		return mid
 
@@ -193,9 +217,11 @@ class Robot:
 		directions = []
 		islandsize = 0
 		min_island_size = 20
+
 		for ind, value in enumerate(binary_map):
 		    if value:
 			islandsize += 1
+
 		    else:
 			if islandsize > min_island_size:
 			    directions.append(ind-islandsize//2-1)
@@ -207,54 +233,103 @@ class Robot:
 		# return midpoint of island closest to angle
 		return min(directions, key = lambda x: abs(x-angle))
 
-	
 
 	def check_path_blocked(self, direction):
 		"""
 		Function to return True if path is blocked in specified
 		direction (+- 15deg). Otherwise returns False
 		"""
-		
+
 		freedom = 15
 		threshold = 4
 		ang = 135 + int(direction)		
+
 		laser_values = self.laser[ang-freedom:ang+freedom]
-		
+
 		# if any value in range < threshold: path is blocked
 		# range is from direction-freedom to direction+freedom
 		if any(l < threshold for l in laser_values):
 			# path is blocked			
 			return True		
+		return False
 		
-		return False		
-						
-		
+
 	def get_angle_to_waypoint(self):
 		"""
 		Get relative angle from robot direction to waypoint (ex: -30
 		means 30 degrees to the right)
 		"""
+
 		ang = atan2(self.waypoint_y-self.robot_y, self.waypoint_x-self.robot_x)
-		
 		ang = (ang - self.robot_theta) * 180 / pi
-		
+
 		if ang > 180:
 			return ang-360
 		if ang < -180:
 			return ang+360
 		return ang
-	
-	def circlemove_init(self):
-		self.circlemove_init_cx = self.robot_x
-		self.circlemove_init_cy = self.robot_y
 
+
+	def circlemove_init(self):
+
+		self.waypoint_x = self.robot_x 
+		self.waypoint_y = self.robot_y
+		self.circlemove_values = []
+		self.circlemove_vcont = 0
+		
+		self.vel_msg.linear.x = 1
+		self.vel_msg.angular.z = 0.5
+
+		
 
 	def circle_move(self):		
 
-		# DO CIRCLE
-		# WHILE DOING CIRCLE CHECK FOR COLISIONS
-		# AFTER CIRCLE (REACHING INITIAL POTITION) GET NEW WAYPOINT		
-		pass
+		# check if i dont need to read anymore (dont need to do full circle: if 
+		# gasvalue increases and then starts decreasing, we dont need more)
+		# move to best direction whlie avoiding walls	
+
+
+		if self.circlemove_vcont % 1000 == 0:
+			self.circlemove_values.append((self.robot_x, self.robot_y, self.gas))
+
+		if self.circlemove_vcont > 100000 and self.check_waypoint(0.4):
+			print('circle move OVER')
+
+			# define new waypoint
+			self.get_waypoint_from_circlemove()
+			self.state = self.GOALSEEK
+
+		self.circlemove_vcont += 1
+
+
+	def get_waypoint_from_circlemove(self):
+
+		# http://paulbourke.net/geometry/circlesphere/
+
+		nvalues = len(self.circlemove_values)
+		x1,y1,_ = self.circlemove_values[0]
+		x2,y2,_ = self.circlemove_values[nvalues//3]
+		x3,y3,_ = self.circlemove_values[2*nvalues//3]
+
+		ma = (y2-y1)/(x2-x1)
+		mb = (y3-y2)/(x3-x2)
+
+		cx = (ma*mb*(y1-y3) + mb*(x1+x2) - ma*(x2+x3)) / (2*(mb-ma))
+		cy = -(1/ma)*(cx-(x1+x2)/2)+(y1+y2)/2
+
+
+		px, py, _ = max(self.circlemove_values, key=lambda val: val[2])
+
+
+		# standard vector equation of line
+		k = 10 # chosen randomly
+		
+		self.waypoint_x = cx + k*(px-cx)
+		self.waypoint_y = cy + k*(py-cy)
+
+		# do same for min circlemove_values and check colinearity and adjust??
+
+
 
 
 if __name__ == '__main__':
@@ -267,7 +342,13 @@ if __name__ == '__main__':
 
 
 
-# TODO: CIRCLE MOVE
+
+
+
+# TODO: waypoint initial cant be 0,0 -> needs to be randoml genenerated (?)
+# TODO: dont need to do full circlemove
+
+
 
 
 # self.move_to_waypoint() LOGIC:
